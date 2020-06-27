b3886b00d43d1a9d40cb70dfbda74e453492961b
Vikash Chauhan <vikashchauhan51@gmail.com>
2020-06-27 10:09:54 +0530

    spalsh

--- UnitConverter/UnitConverter.Android/MainActivity.cs
+++ UnitConverter/UnitConverter.Android/MainActivity.cs
@@ -6,10 +6,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace UnitConverter.Droid
 {
-    [Activity(Label = "UnitConverter", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Unit Converter", Icon = "@mipmap/icon", Theme = "@style/MainTheme", LaunchMode = LaunchMode.SingleTop, MainLauncher = false, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -20,6 +21,13 @@ namespace UnitConverter.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+        public override void OnTrimMemory([GeneratedEnum] TrimMemory level)
+        {
+
+            base.OnTrimMemory(level);
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+            base.OnTrimMemory(level);
         }
     }
 }
---
+++ UnitConverter/UnitConverter.Android/SplashActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace UnitConverter.Droid
+{
+    [Activity(Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashActivity : Activity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            SimulateStartup();
+
+        }
+
+        // Simulates background work that happens behind the splash screen
+        void SimulateStartup() => StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+    }
+}
